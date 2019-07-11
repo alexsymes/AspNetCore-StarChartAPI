dde0e6098079e93f764306ceecc954934d85b72d
AlexSymes <asymes@nanthealth.com>
2019-07-11 14:39:52 +0100

    Second module complete

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using StarChart.Models;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; } 
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Sattelites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
